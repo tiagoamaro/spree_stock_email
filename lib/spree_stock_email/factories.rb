cb24f39e3c8c09e72c76b2b7cdf2f76be18aa9a4
Tiago Amaro <tiago.amaro@rastre.me>
2013-12-05 10:38:33 -0200

    creating a factory for stock_email and creating spec_helper

--- lib/spree_stock_email/factories.rb
+++ lib/spree_stock_email/factories.rb
@@ -3,4 +3,10 @@ FactoryGirl.define do
   #
   # Example adding this to your spec_helper will load these Factories for use:
   # require 'spree_stock_email/factories'
+
+  factory :stock_email, class: Spree::StockEmail do
+    product
+    sequence(:email) { |n| "name#{n}@email.com" }
+    sent_at Time.now
+  end
 end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,63 @@
+# Run Coverage report
+require 'simplecov'
+SimpleCov.start do
+  add_filter 'spec/dummy'
+  add_group 'Controllers', 'app/controllers'
+  add_group 'Helpers', 'app/helpers'
+  add_group 'Mailers', 'app/mailers'
+  add_group 'Models', 'app/models'
+  add_group 'Views', 'app/views'
+  add_group 'Libraries', 'lib'
+end
+
+# Configure Rails Environment
+ENV['RAILS_ENV'] = 'test'
+
+require File.expand_path('../dummy/config/environment.rb',  __FILE__)
+
+require 'rspec/rails'
+require 'database_cleaner'
+require 'ffaker'
+require 'pry'
+require 'capybara/rspec'
+require 'capybara/rails'
+
+# Requires supporting ruby files with custom matchers and macros, etc,
+# in spec/support/ and its subdirectories.
+Dir[File.join(File.dirname(__FILE__), 'support/**/*.rb')].each { |f| require f }
+
+# Requires factories defined in spree_core
+require 'spree/testing_support/factories'
+require 'spree/testing_support/controller_requests'
+require 'spree/testing_support/authorization_helpers'
+require 'spree/testing_support/url_helpers'
+
+# Requires factories defined in lib/spree_stock_email/factories.rb
+require 'spree_stock_email/factories'
+
+RSpec.configure do |config|
+  config.include FactoryGirl::Syntax::Methods
+
+  config.mock_with :rspec
+  config.color = true
+  config.use_transactional_fixtures = false
+
+  # Ensure Suite is set to use transactions for speed.
+  config.before :suite do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with :truncation
+  end
+
+  # Before each spec check if it is a Javascript test and switch between using database transactions or not where necessary.
+  config.before :each do
+    DatabaseCleaner.strategy = example.metadata[:js] ? :truncation : :transaction
+    DatabaseCleaner.start
+  end
+
+  # After each spec clean the database.
+  config.after :each do
+    DatabaseCleaner.clean
+  end
+
+  config.fail_fast = ENV['FAIL_FAST'] || false
+end
